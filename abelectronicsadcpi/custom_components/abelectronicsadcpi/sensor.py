b3d1544c8175e61eaba12782708ad0664c522c97
Andrew Dorey <sales@abelectronics.co.uk>
2022-03-30 18:30:00 +0100

    Updated ADC components to improve performance

--- abelectronicsadcpi/custom_components/abelectronicsadcpi/sensor.py
+++ abelectronicsadcpi/custom_components/abelectronicsadcpi/sensor.py
@@ -2,6 +2,7 @@
 from custom_components.abelectronicsadcpi.ADCPi import ADCPi
 import voluptuous as vol
 import asyncio
+import concurrent.futures
 from datetime import timedelta
 from homeassistant.helpers.entity import Entity
 from homeassistant.components.sensor import PLATFORM_SCHEMA
@@ -17,7 +18,7 @@ CONF_I2C_ADDRESS1 = "i2c_address"
 CONF_I2C_ADDRESS2 = "i2c_address2"
 
 DEFAULT_NAME = 'ADC Pi Sensor'
-DEFAULT_BITRATE = 18
+DEFAULT_BITRATE = 16
 DEFAULT_PGA = 1
 DEFAULT_I2C_ADDRESS1 = 0x68
 DEFAULT_I2C_ADDRESS2 = 0x69
@@ -50,6 +51,25 @@ class abelectronicsadcpiSensor(Entity):
     """
     Control the MCP3424 ADC on the ADC Pi.
     """
+    v1 = 0
+    v2 = 0
+    v3 = 0
+    v4 = 0
+    v5 = 0
+    v6 = 0
+    v7 = 0
+    v8 = 0
+    
+    def getVals(self):
+        self.v1 = self._adc.read_voltage(1)
+        self.v2 = self._adc.read_voltage(2)
+        self.v3 = self._adc.read_voltage(3)
+        self.v4 = self._adc.read_voltage(4)
+        self.v5 = self._adc.read_voltage(5)
+        self.v6 = self._adc.read_voltage(6)
+        self.v7 = self._adc.read_voltage(7)
+        self.v8 = self._adc.read_voltage(8)
+
     def __init__(self, adc):
         """Initialize the sensor."""
         self._state = None
@@ -76,20 +96,21 @@ class abelectronicsadcpiSensor(Entity):
         return 'V'
 
     @property
-    def device_state_attributes(self):
+    def extra_state_attributes(self):
         """Return the state attributes of the device."""
         attr = {}
-        attr['input1'] = self._adc.read_voltage(1)
-        attr['input2'] = self._adc.read_voltage(2)
-        attr['input3'] = self._adc.read_voltage(3)
-        attr['input4'] = self._adc.read_voltage(4)
-        attr['input5'] = self._adc.read_voltage(5)
-        attr['input6'] = self._adc.read_voltage(6)
-        attr['input7'] = self._adc.read_voltage(7)
-        attr['input8'] = self._adc.read_voltage(8)
+        attr['input1'] = self.v1
+        attr['input2'] = self.v2
+        attr['input3'] = self.v3
+        attr['input4'] = self.v4
+        attr['input5'] = self.v5
+        attr['input6'] = self.v6
+        attr['input7'] = self.v7
+        attr['input8'] = self.v8
         return attr
 
     @asyncio.coroutine
     def async_update(self):
-        """Fetch new state data."""
+        """Fetch new state data."""        
+        self.getVals()
         self._state = self._state
